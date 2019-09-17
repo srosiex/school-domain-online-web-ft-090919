15d06baff914dec1163d2e5f9e29a4535554127b
Sarah H <sarahrose.henderson@gmail.com>
2019-09-17 17:30:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 class School
   def initialize(name)
     @name
-
+roster = {}
   end
   def name
     @name
   end
-  ROSTER = []
+
   def roster=(roster)
     @roster = roster
     ROSTER << roster
